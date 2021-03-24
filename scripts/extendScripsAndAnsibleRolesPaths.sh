27fb687b674cf6aea37da8278696ffaed18930f1
AlexandrSokolov <sav.public@yandex.com>
2021-03-24 19:12:00 +0100

    corrected properties extracting, improved output

--- scripts/extendScripsAndAnsibleRolesPaths.sh
+++ scripts/extendScripsAndAnsibleRolesPaths.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # copy all shared roles
-rm -rf ~/.ansible/common_roles
-mkdir -p ~/.ansible/common_roles
-cp -r roles/* ~/.ansible/common_roles
+rm -rf ~/.ansible/common_roles && \
+mkdir -p ~/.ansible/common_roles && \
+cp -r roles/* ~/.ansible/common_roles && \
+echo "SUCCESS: All common roles have been copied from \`roles\` into \`~/.ansible/common_roles\` folder."
 
-cp -r files ~/.ansible/
+cp -r files ~/.ansible/ && \
+echo "SUCCESS: The \`files\` folder has been copied into \`~/.ansible\` folder."
 
 ./scripts/extendScriptsPath.sh
